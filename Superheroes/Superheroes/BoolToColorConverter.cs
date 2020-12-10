3e1e776371babbd7bc3868f06f262d748d17d96d
LGCaparros <lg.caparros@iesdoctorbalmis.com>
2020-12-10 09:54:53 +0100

    Segundo commit

---
+++ Superheroes/Superheroes/BoolToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Superheroes
+{
+    class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Superheroe superheroe = (Superheroe)value;
+            if (superheroe.Heroe)
+                return "PaleGreen";
+            else
+                return "IndianRed";
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Superheroes/Superheroes/pestañaSuperheroes.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para pestañaSuperheroes.xaml
+    /// </summary>
+    public partial class pestañaSuperheroes : UserControl
+    {
+        
+        public pestañaSuperheroes()
+        {
+            InitializeComponent();
+            List<Superheroe> superheroes = Superheroe.GetSamples();
+            superheroesDockPanel.DataContext = superheroes;
+            
+        }
+
+    }
+}
